1856332724a9bfa5fac14a30a2b980e1878f3500
Michael Dickinson <michael.dickinson3949@gmail.com>
2025-10-19 01:51:23 -0700

    squareish viewport for screenies

--- backend/testjs/playwright.config.js
+++ backend/testjs/playwright.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "@playwright/test"
 
 export default defineConfig({
   use: {
+    viewport: { width: 1200, height: 1000 },
     // headless: false,
     // launchOptions: {
     //   slowMo: 100, // 👈 slows every action by 250 ms
