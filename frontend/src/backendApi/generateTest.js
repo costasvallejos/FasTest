b808167cdab7bc821535a8fe56731f1b97480c73
Michael Dickinson <michael.dickinson3949@gmail.com>
2025-10-18 19:54:54 -0700

    use dummy data that takes a minute to work

--- frontend/src/backendApi/generateTest.js
+++ frontend/src/backendApi/generateTest.js
@@ -1,20 +1,27 @@
-import {TestGenerationResponseSchema,  TestGenerationRequestSchema } from "../schemas/generateTestSchema.js"
+import {
+  TestGenerationResponseSchema,
+  TestGenerationRequestSchema,
+} from "../schemas/generateTestSchema.js"
 import { API_BASE_URL } from "../constants.js"
-// import { getDummyTestGenerationResponse } from "../test_data/dummyData.js"
+import { getDummyTestGenerationResponse } from "../test_data/dummyData.js"
 
 export const generateTest = async (requestData) => {
   TestGenerationRequestSchema.parse(requestData)
 
   // !!! USING DUMMY DATA
-  // const responseData = getDummyTestGenerationResponse()
-  const response = await fetch(`${API_BASE_URL}/generate-test`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(requestData),
-  })
-  const responseData = await response.json()
+  const responseData = getDummyTestGenerationResponse()
+
+  // Simulate network delay
+  await new Promise((resolve) => setTimeout(resolve, 20000))
+
+  // const response = await fetch(`${API_BASE_URL}/generate-test`, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(requestData),
+  // })
+  // const responseData = await response.json()
 
   TestGenerationResponseSchema.parse(responseData)
 
